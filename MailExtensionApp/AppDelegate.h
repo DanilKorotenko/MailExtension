e9d106f16c6e16e5bd3ed1091960ad3b033024f7
Danil Korotenko <danil.korotenko@gmail.com>
2023-05-19 14:29:56 +0300

    Container app was added.

---
+++ MailExtensionApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MailExtensionApp
+//
+//  Created by Danil Korotenko on 5/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
